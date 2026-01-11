8ad650d316345c5aa36816f197ac95bf02f4a8d0
perez <perezalexandre430@gmail.com>
2026-01-11 17:09:50 +0100

    all projects

--- apps/nuxt/src/server/api/projects.ts
+++ apps/nuxt/src/server/api/projects.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async () => {
   const BASEROW_TOKEN = 'DfZJPxNKrDKiPlrtNuKjbdZRUFU8h6AG'
   const TABLE_ID = '790592'
 
-  const response = await $fetch(`https://api.baserow.io/api/database/rows/table/${TABLE_ID}/?user_field_names=true`, {
+  const response = await $fetch(`https://api.baserow.io/api/database/rows/table/${TABLE_ID}/?user_field_names=true&size=200`, {
     headers: {
       Authorization: `Token ${BASEROW_TOKEN}`
     }
